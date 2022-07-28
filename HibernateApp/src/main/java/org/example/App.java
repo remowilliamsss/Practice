1a608bf89d9e3bebb7adb5f517e872f7863807f7
Viktor Egorov <vitka318@ya.ru>
2022-07-28 23:05:42 +0300

    practice 28.07.22

--- HibernateApp/src/main/java/org/example/App.java
+++ HibernateApp/src/main/java/org/example/App.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import org.example.model.Actor;
+import org.example.model.Movie;
 import org.example.model.Principal;
 import org.example.model.School;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Hello world!
@@ -12,50 +18,24 @@ import org.hibernate.cfg.Configuration;
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
-                .addAnnotatedClass(Principal.class).addAnnotatedClass(School.class);
+                .addAnnotatedClass(Actor.class).addAnnotatedClass(Movie.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
 
-        try {
+        try (sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
+            Actor actor = session.get(Actor.class, 2);
+            System.out.println(actor.getMovies());
 
-            // получите любого директора, а затем получите его школу
-            Principal principal = session.get(Principal.class, 1);
-            System.out.println(principal.getName());
+            Movie movieToRemove = actor.getMovies().get(0);
 
-            School school = principal.getSchool();
-            System.out.println(school.getSchoolNumber());
-
-            // получите любую школу, а затем получите ее директора
-            school = session.get(School.class, 4);
-            System.out.println(school.getSchoolNumber());
-
-            principal = school.getPrincipal();
-            System.out.println(principal.getName());
-
-            // создайте нового директора и новую школу и свяжите эти сущности
-            Principal newPrincipal = new Principal("Viktor", 26);
-            School newSchool = new School(777);
-
-            newPrincipal.setSchool(newSchool);
-            session.save(newPrincipal);
-
-            // смените директора у существующей школы
-            newPrincipal = new Principal("Alex", 43);
-            session.save(newPrincipal);
-
-            school.setPrincipal(newPrincipal);
-            System.out.println(school.getPrincipal().getName());
-
-            // попробуйте добавить вторую школу для существующего директора и изучите возникающую ошибку
-            // school.setPrincipal(session.get(Principal.class, 5));
+            actor.getMovies().remove(0);
+            movieToRemove.getActors().remove(actor);
 
             session.getTransaction().commit();
 
-        } finally {
-            sessionFactory.close();
         }
     }
 }

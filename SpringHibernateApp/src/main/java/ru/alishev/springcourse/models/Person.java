709b24146a07cb1a31b99d583f5eb4b06f664482
Viktor Egorov <vitka318@ya.ru>
2022-08-04 13:48:12 +0300

    update SpringHibernateApp

--- SpringHibernateApp/src/main/java/ru/alishev/springcourse/models/Person.java
+++ SpringHibernateApp/src/main/java/ru/alishev/springcourse/models/Person.java
@@ -1,6 +1,7 @@
 package ru.alishev.springcourse.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -24,6 +25,11 @@ public class Person {
     @Min(value = 0, message = "Age should be greater than 0")
     @Column(name = "age")
     private int age;
+
+    @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
 
     public Person() {
 
@@ -56,6 +62,14 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override

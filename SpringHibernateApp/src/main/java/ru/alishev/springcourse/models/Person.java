62cf8b8b778238d4cd28eeef24c5d45d33041841
Viktor Egorov <vitka318@ya.ru>
2022-08-05 20:46:41 +0300

    update PeopleRepository, add ItemsRepository and ItemsService

--- SpringHibernateApp/src/main/java/ru/alishev/springcourse/models/Person.java
+++ SpringHibernateApp/src/main/java/ru/alishev/springcourse/models/Person.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * @author Neil Alishev
@@ -30,6 +31,9 @@ public class Person {
     @NotEmpty(message = "Email should not be empty")
     @Email
     private String email;
+
+    @OneToMany(mappedBy = "owner")
+    private List<Item> items;
 
     public Person() {
 
@@ -70,6 +74,14 @@ public class Person {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
     }
 
     @Override

406c6231288b957e2293d91c98c1d37ea0cf9c89
Viktor Egorov <vitka318@ya.ru>
2022-09-12 20:59:52 +0300

    initial commit + update application.properties

---
+++ ProjectTwoBoot/src/main/java/ru/egorov/springcourse/ProjectTwoBoot/ProjectTwoBootApplication.java
@@ -0,0 +1,13 @@
+package ru.egorov.springcourse.ProjectTwoBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTwoBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTwoBootApplication.class, args);
+	}
+
+}

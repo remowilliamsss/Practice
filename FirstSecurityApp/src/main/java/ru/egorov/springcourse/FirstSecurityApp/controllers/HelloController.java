64e381d0bc4c9e919b8cce4e1c696a6c635de4bd
Viktor Egorov <vitka318@ya.ru>
2022-10-05 23:11:20 +0300

    practice 05.10 authentication

--- FirstSecurityApp/src/main/java/ru/egorov/springcourse/FirstSecurityApp/controllers/HelloController.java
+++ FirstSecurityApp/src/main/java/ru/egorov/springcourse/FirstSecurityApp/controllers/HelloController.java
@@ -1,12 +1,24 @@
 package ru.egorov.springcourse.FirstSecurityApp.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.egorov.springcourse.FirstSecurityApp.security.PersonDetails;
 
 @Controller
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
+        return "hello";
+    }
+
+    @GetMapping("/showUserInfo")
+    public String showUserInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        PersonDetails personDetails = (PersonDetails) authentication.getPrincipal();
+        System.out.println(personDetails.getPerson());
+
         return "hello";
     }
 }

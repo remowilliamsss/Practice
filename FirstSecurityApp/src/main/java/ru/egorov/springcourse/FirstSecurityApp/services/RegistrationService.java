3e159ea2c06bc31e5df958b6ff261f0dd0a0f384
Viktor Egorov <vitka318@ya.ru>
2022-10-09 23:04:38 +0400

    practice 09.10 added BCrypt

--- FirstSecurityApp/src/main/java/ru/egorov/springcourse/FirstSecurityApp/services/RegistrationService.java
+++ FirstSecurityApp/src/main/java/ru/egorov/springcourse/FirstSecurityApp/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package ru.egorov.springcourse.FirstSecurityApp.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.egorov.springcourse.FirstSecurityApp.models.Person;
@@ -10,14 +11,17 @@ import ru.egorov.springcourse.FirstSecurityApp.repositories.PeopleRepository;
 public class RegistrationService {
 
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepository.save(person);
     }
 }

ca67796533911bb11b556448857d08fa81b35d3a
Viktor Egorov <vitka318@ya.ru>
2022-10-10 12:08:51 +0400

    practice 10.10 added csrf

--- FirstSecurityApp/src/main/java/ru/egorov/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ FirstSecurityApp/src/main/java/ru/egorov/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -25,8 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // конфигурируем сам Spring Security
         // конфигурируем авторизацию
-        http.csrf().disable() // отключаем защиту от межсайтовой подделки запросов
-                .authorizeRequests()
+        http.authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().authenticated()
                 .and()

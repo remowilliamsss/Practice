9648028be5da943f42e2be0c4ba9decd143a9fbb
Viktor Egorov <vitka318@ya.ru>
2022-10-12 19:32:10 +0400

    practice 12.10 added DTO

--- FirstRestApp/src/main/java/ru/egorov/springcourse/FirstRestApp/controllers/PeopleController.java
+++ FirstRestApp/src/main/java/ru/egorov/springcourse/FirstRestApp/controllers/PeopleController.java
@@ -1,12 +1,14 @@
 package ru.egorov.springcourse.FirstRestApp.controllers;
 
 import org.apache.catalina.connector.Response;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
+import ru.egorov.springcourse.FirstRestApp.dto.PersonDTO;
 import ru.egorov.springcourse.FirstRestApp.models.Person;
 import ru.egorov.springcourse.FirstRestApp.services.PeopleService;
 import ru.egorov.springcourse.FirstRestApp.util.PersonErrorResponse;
@@ -14,31 +16,36 @@ import ru.egorov.springcourse.FirstRestApp.util.PersonNotCreatedException;
 import ru.egorov.springcourse.FirstRestApp.util.PersonNotFoundException;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/people")
 public class PeopleController {
 
     private final PeopleService peopleService;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public PeopleController(PeopleService peopleService) {
+    public PeopleController(PeopleService peopleService, ModelMapper modelMapper) {
         this.peopleService = peopleService;
+        this.modelMapper = modelMapper;
     }
 
     @GetMapping()
-    public List<Person> getPeople() {
-        return peopleService.findAll(); // Jackson конвертирует эти объекты в JSON
+    public List<PersonDTO> getPeople() {
+        return peopleService.findAll().stream().map(this::convertToPersonDTO)
+                .collect(Collectors.toList()); // Jackson конвертирует эти объекты в JSON
     }
 
     @GetMapping("/{id}")
-    public Person getPerson(@PathVariable("id") int id) {
-        return peopleService.findOne(id); // Jackson конвертирует в JSON
+    public PersonDTO getPerson(@PathVariable("id") int id) {
+        return convertToPersonDTO(peopleService.findOne(id)); // Jackson конвертирует в JSON
     }
 
     @PostMapping
-    public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person,
+    public ResponseEntity<HttpStatus> create(@RequestBody @Valid PersonDTO personDTO,
                                              BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             StringBuilder errorMsg = new StringBuilder();
@@ -53,7 +60,7 @@ public class PeopleController {
             throw new PersonNotCreatedException(errorMsg.toString());
         }
 
-        peopleService.save(person);
+        peopleService.save(convertToPerson(personDTO));
 
         // отправляем HTTP-ответ с пустым телом и со статусом 200
         return ResponseEntity.ok(HttpStatus.OK);
@@ -79,5 +86,13 @@ public class PeopleController {
 
         // В HTTP-ответе тело response и статус в заголовке
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    private Person convertToPerson(PersonDTO personDTO) {
+        return modelMapper.map(personDTO, Person.class);
+    }
+
+    private PersonDTO convertToPersonDTO(Person person) {
+        return modelMapper.map(person, PersonDTO.class);
     }
 }
